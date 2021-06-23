61deb77f9de538439568c78b31ce066dff60842b
vsrin008 <zapperwing@gmail.com>
2021-06-23 16:48:51 -0700

    Trimmed some code

--- foreground.js
+++ foreground.js
@@ -1,1 +1,6 @@
-console.log(chrome.storage.local.get('className'));
+var nameOfClass = chrome.storage.local.get('className', data);
+
+console.log("Foreground.js is running");
+console.log(nameOfClass, "lol");
+document.querySelector("body > div.L3eUgb > div.o3j99.LLD4me.yr19Zb.LS8OJ > div > img").style.transitionDuration = '3.0s';
+document.querySelector("body > div.L3eUgb > div.o3j99.LLD4me.yr19Zb.LS8OJ > div > img").style.transform = 'rotate(1000deg)';
